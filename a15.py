64e4cf996b5bbf42161cb0cd897cb855892d496d
Joel Sujo <294863925+Joel-Sujo@users.noreply.github.com>
2026-09-12 20:32:59 +0530

    Add files via upload

---
+++ a15.py
@@ -0,0 +1,7 @@
+#Write a Python program to replace a word in a string with another word. 
+
+x = input("String: ")
+y = input("Word to be replaced: ")
+z = input("Word to replace with: ")
+k = x.replace(y,z)
+print("New string: ",k)
